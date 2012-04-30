5433e6dcd8d644b33b3a217a0b2ee405f5856b06
heldplayer <mitchel.pyl@gmail.com>
2012-04-30 23:54:31 +0200

    Added _very_ early user and channel classes (do not work for now)

---
+++ bIRCh/src/me/heldplayer/bIRCh/Channel.java
@@ -0,0 +1,11 @@
+package me.heldplayer.bIRCh;
+
+/**
+ * @author heldplayer
+ *         Represents an IRC channel
+ */
+public class Channel {
+	public String getName() {
+		return "";
+	}
+}
---
+++ bIRCh/src/me/heldplayer/bIRCh/IrcUser.java
@@ -0,0 +1,15 @@
+package me.heldplayer.bIRCh;
+
+/**
+ * @author heldplayer
+ *         Represents a user in an IRC channel
+ */
+public class IrcUser {
+	public String getDisplayName() {
+		return "";
+	}
+
+	public String getName() {
+		return "";
+	}
+}
